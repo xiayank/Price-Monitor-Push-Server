8a474acb2939962a439c97444e12fde19cf41319
Yan Xia <yan.xia.cs@gmail.com>
2017-06-11 23:33:54 -0500

    init commit

---
+++ src/main/java/PushServerMain.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by NIC on 6/11/17.
+ */
+public class PushServerMain {
+
+    public static void main(String args[]) throws IOException, TimeoutException {
+
+
+
+    }
+}
---
+++ src/main/java/product/Product.java
@@ -0,0 +1,20 @@
+package product;
+
+import java.io.Serializable;
+
+/**
+ * Created by NIC on 6/5/17.
+ */
+public class Product implements Serializable {
+
+
+    public String productId;
+    public String detailUrl;
+    public double newPrice;
+    public String title;
+    public String category;
+    public double oldPrice;
+
+
+
+}
